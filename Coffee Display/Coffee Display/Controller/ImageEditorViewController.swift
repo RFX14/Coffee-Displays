7949ae86610a3bc01af5e04664689f7b7ea20e27
Brosales13 <Brosales1313@gmail.com>
2023-03-18 17:36:29 -0500

    basic table view

---
+++ Coffee Display/Coffee Display/Controller/ImageEditorViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ImageEditorViewController.swift
+//  Coffee Display
+//
+//  Created by Brian Rosales on 3/18/23.
+//
+
+import UIKit
+
+class ImageEditorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let myArray: NSArray = ["First","Second","Third"]
+    private var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
+    }
+    
+    //prints info for each cell that is selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myArray[indexPath.row])")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+}
