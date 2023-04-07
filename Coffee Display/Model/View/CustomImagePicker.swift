fc4e1d375a0b2e784c73907e86c76ab0fa42faed
Brosales13 <Brosales1313@gmail.com>
2023-04-07 10:26:54 -0500

    basic crop feature

--- Coffee Display/Model/View/CustomImagePicker.swift
+++ Coffee Display/Model/View/CustomImagePicker.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 import PhotosUI
+
 extension View {
     @ViewBuilder
     func cropImagePicker(options: [Crop], show: Binding<Bool>, croppedImage: Binding<UIImage?>) -> some View {
         CustomImagePicker(options: options, show: show, croppedImage: croppedImage) {
             self
         }
+    }
+    // For making is simple and easy to use
+    @ViewBuilder
+    func frame(_ size: CGSize) -> some View {
+        self
+            .frame(width: size.width, height: size.height)
     }
 }
 
@@ -28,17 +35,122 @@ fileprivate struct CustomImagePicker<Content: View>: View {
         self.options = options
     }
     
+    // View Properties
     @State private var photosItem: PhotosPickerItem?
+    @State private var selectedImage: UIImage?
+    @State private var showDialog: Bool = false
+    @State private var selectedCropType: Crop = .circle
+    @State private var showCropView: Bool = false
+    
     var body: some View {
         content
             .photosPicker(isPresented: $show, selection: $photosItem)
+            .onChange(of: photosItem) { newValue in
+                //Extracting Image
+                if let newValue {
+                    Task {
+                        if let imageData = try? await newValue.loadTransferable(type: Data.self), let image = UIImage(data: imageData) {
+                            // UI Must be updated on Main Thread.
+                            await MainActor.run(body: {
+                                selectedImage = image
+                                showDialog.toggle()
+                            })
+                        }
+                    }
+                }
+            }
+            .confirmationDialog("", isPresented: $showDialog) {
+                //Display All the options
+                ForEach(options.indices, id: \.self) {index in
+                    Button(options[index].name()) {
+                        selectedCropType = options[index]
+                        showCropView.toggle()
+                    }
+                }
+            }
+            .fullScreenCover(isPresented: $showCropView) {
+            //Whenever the sheet is closed, set selectedImage to Null
+                selectedImage = nil
+            } content: {
+                CropView(crop: selectedCropType, image: selectedImage) { croppedImage, status in
+                    
+                }
+            }
     }
 }
 
+struct CropView: View {
+    var crop: Crop
+    var image: UIImage?
+    // This callback will give the cropped image and result status when the checkmark button is pressed
+    var onCrop: (UIImage?, Bool) ->()
+    
+    var body: some View {
+        NavigationStack {
+            ImageView()
+                .navigationTitle("Crop View")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background {
+                    Color.black
+                        .ignoresSafeArea()
+                }
+        }
+    }
+    // Image View
+    @ViewBuilder
+    func ImageView() -> some View {
+        let cropSize = crop.size()
+        GeometryReader {
+            let size = $0.size
+            
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(size)
+            }
+        }
+        .overlay(content: {
+            Grids()
+        })
+        .frame(cropSize)
+        .cornerRadius(crop == .circle ? cropSize.height / 2 : 0)
+    }
+    
+    @ViewBuilder
+    func Grids() -> some View {
+        ZStack {
+            HStack {
+                ForEach(1...5, id: \.self) { _ in
+                    Rectangle()
+                        .fill(.white.opacity(0.7))
+                        .frame(width: 1)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            VStack {
+                ForEach(1...8, id: \.self) { _ in
+                    Rectangle()
+                        .fill(.white.opacity(0.7))
+                        .frame(height: 1)
+                        .frame(maxHeight: .infinity)
+                    
+                }
+            }
+        }
+    }
+}
 
- struct CustomImagePicker_Previews: PreviewProvider {
-     static var previews: some View {
-         ImageEditor(manager: ScreenManager(), selectedScreen: Screen(name: "screen_1", items:[Coffee_Display.BasicItem(title: "item 1", price: "$1300", description: Optional("blah"), position: 0), Coffee_Display.BasicItem(title: "Churro", price: "$1000", description: Optional("Sugar"), position: 1)]))
-     }
- }
+struct CustomImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CropView(crop: .circle, image: UIImage(named: "Sample Pic")) { _, _ in
+            
+        }
+    }
+    /*
+    ImageEditor(manager: ScreenManager(), selectedScreen: Screen(name: "screen_1", items:[Coffee_Display.BasicItem(title: "item 1", price: "$1300", description: Optional("blah"), position: 0), Coffee_Display.BasicItem(title: "Churro", price: "$1000", description: Optional("Sugar"), position: 1)]))
+     */
+}
 
